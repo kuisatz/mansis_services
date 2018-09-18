7bc1ec50bb9f9471b8720accbe7fa2c00a734dda
Oki Oki <ociran@gmail.com>
2018-09-18 16:30:54 +0300

    roo tid

--- mansissa_slim_v1/sysaccbodydeff.php
+++ mansissa_slim_v1/sysaccbodydeff.php
@@ -343,7 +343,9 @@ $app->get("/pkUpdateMakeActiveOrPassive_sysaccbodydeff/", function () use ($app 
     $jsonFilter = json_decode($user, true); 
                     if ($jsonFilter != null) {
                       print_r("<<<<<<<<<<<<<<<<<<<<" );  
-                        print_r($jsonFilter ["Id"]);  
+                        print_r($jsonFilter ["Id"]);   
+                          print_r(">>>>>>>>><<<<<<<<<<<" );  
+                            print_r($jsonFilter ["RootId"]);   
                           print_r(">>>>>>>>>>>>>>>>>" );  
     } 
    
