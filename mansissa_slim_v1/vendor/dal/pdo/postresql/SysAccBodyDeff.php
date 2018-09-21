5c4868b6f302e34e2e4d1fd08ba8a411ef17a18a
Oki Oki <ociran@gmail.com>
2018-09-21 14:41:54 +0300

    accBodyDeffDdList

--- mansissa_slim_v1/vendor/dal/pdo/postresql/SysAccBodyDeff.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/SysAccBodyDeff.php
@@ -528,7 +528,7 @@ class SysAccBodyDeff extends \DAL\DalSlim {
                 ORDER BY  id  
                                  " ;
              $statement = $pdo->prepare($sql);
-                //  echo debugPDO($sql, $params);
+             echo debugPDO($sql, $params);
             $statement->execute();
             $result = $statement->fetchAll(\PDO::FETCH_ASSOC); 
             $errorInfo = $statement->errorInfo();
