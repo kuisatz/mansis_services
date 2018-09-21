9a2a2eeee8d4bb6c9ab62bbd07cf4668acac6bd1
Oki Oki <ociran@gmail.com>
2018-09-21 10:06:43 +0300

    test

--- mansissa_slim_v1/sysaccbodydeff.php
+++ mansissa_slim_v1/sysaccbodydeff.php
@@ -344,6 +344,7 @@ $app->get("/pkUpdateMakeActiveOrPassive_sysaccbodydeff/", function () use ($app 
  //   echo "Server is running: ".$redis->ping(); 
     $rid =  'testInstance7794f89a-59a3-44f8-b2f8-1e44dc8a6f34';
     $user = $statement->hGetAll($rid);
+    echo "Server is running: ".$statement->ping(); 
   
     print_r($user);
     if (isset($user['data']) && $user['data'] != "") {
