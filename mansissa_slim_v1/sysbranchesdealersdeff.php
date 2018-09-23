47019329d14996408679559366eeb35b88cc0bf5
Oki Oki <ociran@gmail.com>
2018-09-24 01:29:58 +0300

    pkFillAccBodyDeffGridx_sysbranchesdealersdeff

--- mansissa_slim_v1/sysbranchesdealersdeff.php
+++ mansissa_slim_v1/sysbranchesdealersdeff.php
@@ -45,7 +45,7 @@ $app->add(new \Slim\Middleware\MiddlewareServiceManager());
 
 /**
  *  * Okan CIRAN
- * @since 05.05.2016
+ * @since 15-08-2018
  */
 $app->get("/pkBranchesDealersDeffDdList_sysbranchesdealersdeff/", function () use ($app ) {
     $stripper = $app->getServiceManager()->get('filterChainerCustom');
@@ -95,6 +95,310 @@ $app->get("/pkBranchesDealersDeffDdList_sysbranchesdealersdeff/", function () us
     $app->response()->header("Content-Type", "application/json");
     $app->response()->body(json_encode($flows));
 });
+
+
+/**
+ *  * Okan CIRAN
+ * @since 15-08-2018
+ */
+$app->get("/pkFillAccBodyDeffGridx_sysbranchesdealersdeff/", function () use ($app ) {
+    $stripper = $app->getServiceManager()->get('filterChainerCustom');
+    $stripChainerFactory = new \Services\Filter\Helper\FilterChainerFactory();
+    $BLL = $app->getBLLManager()->get('sysBranchesDealersDeffBLL');
+    $headerParams = $app->request()->headers();
+    if (!isset($headerParams['X-Public']))
+        throw new Exception('rest api "pkFillAccBodyDeffGridx_sysbranchesdealersdeff" end point, X-Public variable not found');
+    $pk = $headerParams['X-Public'];
+
+    $vLanguageCode = 'en';
+    if (isset($_GET['language_code'])) {
+        $stripper->offsetSet('language_code', $stripChainerFactory->get(stripChainers::FILTER_ONLY_LANGUAGE_CODE, $app, $_GET['language_code']));
+    }  
+    $vPage = NULL;
+    if (isset($_GET['page'])) {
+        $stripper->offsetSet('page', $stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED, $app, $_GET['page']));
+    }
+    $vRows = NULL;
+    if (isset($_GET['rows'])) {
+        $stripper->offsetSet('rows', $stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED, $app, $_GET['rows']));
+    }
+    $vSort = NULL;
+    if (isset($_GET['sort'])) {
+        $stripper->offsetSet('sort', $stripChainerFactory->get(stripChainers::FILTER_PARANOID_LEVEL2, $app, $_GET['sort']));
+    }
+    $vOrder = NULL;
+    if (isset($_GET['order'])) {
+        $stripper->offsetSet('order', $stripChainerFactory->get(stripChainers::FILTER_ONLY_ORDER, $app, $_GET['order']));
+    }
+    $filterRules = null;
+    if (isset($_GET['filterRules'])) {
+        $stripper->offsetSet('filterRules', $stripChainerFactory->get(stripChainers::FILTER_PARANOID_JASON_LVL1, $app, $_GET['filterRules']));
+    } 
+    $lid = null;
+    if (isset($_GET['lid'])) {
+         $stripper->offsetSet('lid',$stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED,
+                                                $app,
+                                                $_GET['lid']));
+    }
+    $stripper->strip();
+    if($stripper->offsetExists('lid')) $lid = $stripper->offsetGet('lid')->getFilterValue();
+    if ($stripper->offsetExists('language_code')) $vLanguageCode = $stripper->offsetGet('language_code')->getFilterValue();    
+ 
+    if ($stripper->offsetExists('page')) { $vPage = $stripper->offsetGet('page')->getFilterValue(); }
+    if ($stripper->offsetExists('rows')) { $vRows = $stripper->offsetGet('rows')->getFilterValue(); }
+    if ($stripper->offsetExists('sort')) { $vSort = $stripper->offsetGet('sort')->getFilterValue(); }
+    if ($stripper->offsetExists('order')) { $vOrder = $stripper->offsetGet('order')->getFilterValue(); }
+    if ($stripper->offsetExists('filterRules')) { $filterRules = $stripper->offsetGet('filterRules')->getFilterValue(); } 
+
+    $resDataGrid = $BLL->fillAccBodyDeffGridx(array(
+        'language_code' => $vLanguageCode,
+        'LanguageID' => $lid,
+        'page' => $vPage,
+        'rows' => $vRows,
+        'sort' => $vSort,
+        'order' => $vOrder,
+ 
+        'filterRules' => $filterRules,
+        'pk' => $pk,
+    ));
+   
+    $resTotalRowCount = $BLL->fillAccBodyDeffGridxRtl(array(
+        'language_code' => $vLanguageCode, 
+        'LanguageID' => $lid,
+ 
+        'filterRules' => $filterRules,
+        'pk' => $pk,
+    ));
+    $counts=0;
+  
+    $menu = array();            
+    if (isset($resDataGrid[0]['id'])) {      
+        foreach ($resDataGrid as $menu) {
+            $menus[] = array(
+               "id" => $menu["id"],
+                "apid" => intval($menu["apid"]),  
+                "name" => html_entity_decode($menu["name"]), 
+               
+                "branch_no" => html_entity_decode($menu["branch_no"]),   
+                "address1" => html_entity_decode($menu["address1"]),   
+                "address2" => html_entity_decode($menu["address2"]),   
+                "address3" => html_entity_decode($menu["address3"]),   
+                "postalcode" => html_entity_decode($menu["postalcode"]),   
+                "country_name" => html_entity_decode($menu["country_name"]),   
+                "region_name" => html_entity_decode($menu["region_name"]),   
+                "city_name" => html_entity_decode($menu["city_name"]),  
+                "departman_name" => html_entity_decode($menu["departman_name"]),  
+                "country_id" =>  ($menu["country_id"]),   
+                "country_region_id" =>  ($menu["country_region_id"]),   
+                "city_id" =>  ($menu["city_id"]),   
+                "sis_department_id" =>  ($menu["sis_department_id"]),   
+                  
+                "op_username" => html_entity_decode($menu["op_user_name"]), 
+                "state_active" => html_entity_decode($menu["state_active"]),       
+                "date_saved" => $menu["date_saved"],
+                "date_modified" => $menu["date_modified"],  
+                "language_code" => $menu["language_code"],
+                "active" => $menu["active"], 
+                "op_user_id" => $menu["op_user_id"], 
+                "language_id" => $menu["language_id"],
+                "language_name" =>html_entity_decode( $menu["language_name"]), 
+                 
+            );
+        }
+       $counts = $resTotalRowCount[0]['count'];
+      } ELSE  $menus = array();       
+
+    $app->response()->header("Content-Type", "application/json");
+    $resultArray = array();
+    $resultArray['totalCount'] = $counts;
+    $resultArray['items'] = $menus;
+    $app->response()->body(json_encode($resultArray));
+});
+
+/**x
+ *  * Okan CIRAN
+ * @since 15-08-2018
+ */
+$app->get("/pkUpdateMakeActiveOrPassive_sysbranchesdealersdeff/", function () use ($app ) {
+    $RedisConnect = $app->getServiceManager()->get('redisConnectFactory');
+ 
+    $rid = 'testInstance7794f89a-59a3-44f8-b2f8-1e44dc8a6f34';
+    $user = $RedisConnect->hGetAll($rid);
+    //   echo "Server is running: ".$RedisConnect->ping(); 
+
+ //   print_r($user);
+    if (isset($user['data']) && $user['data'] != "") {
+        $user = trim($user['data']);
+
+        $jsonFilter = json_decode($user, true);
+        if ($jsonFilter != null) {
+    //        print_r("<<<<<<<<<<<<<<<<<<<<");
+            if (isset($jsonFilter['Id'])) {
+           //     print_r($jsonFilter ["Id"]);
+            }
+    ///        print_r(">>>>>>>>><<<<<<<<<<<");
+            if (isset($jsonFilter['RootId'])) {
+      //          print_r($jsonFilter ["RootId"]);
+            }
+      //      print_r(">>>>>>>>>>>>>>>>>");
+            if (isset($jsonFilter['RoleId'])) {
+       //         print_r($jsonFilter ["RoleId"]);
+            }
+        //    print_r(">>>>>>>>>>>>>>>>>");
+        }
+    } else {
+       //  print_r("<<<<<<<3ee3>>>>>>>");
+         //  print_r("<<<<<<<123>>>>>>>"); 
+        ;
+    }
+ 
+    $stripper = $app->getServiceManager()->get('filterChainerCustom');
+    $stripChainerFactory = new \Services\Filter\Helper\FilterChainerFactory();
+    $BLL = $app->getBLLManager()->get('sysBranchesDealersDeffBLL');
+
+    
+    $RedisConnect = $app->getServiceManager()->get('redisConnectFactory');
+      
+    $headerParams = $app->request()->headers();
+    $Pk = $headerParams['X-Public'];
+ //   $user = $RedisConnect->hGetAll($Pk);
+
+    $vId = NULL;
+    if (isset($_GET['id'])) {
+        $stripper->offsetSet('id', $stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED, $app, $_GET['id']));
+    }
+
+    $stripper->strip();
+    if ($stripper->offsetExists('id')) {
+        $vId = $stripper->offsetGet('id')->getFilterValue();
+    }
+
+    $resData = $BLL->makeActiveOrPassive(array(
+        'id' => $vId,
+        'pk' => $Pk,
+    ));
+
+    $app->response()->header("Content-Type", "application/json");
+    $app->response()->body(json_encode($resData));
+}
+);
+
+/**
+ *  * Okan CIRAN
+ * @since 15-08-2018
+ */ 
+$app->get("/pkInsertAct_sysbranchesdealersdeff/", function () use ($app ) {
+    $stripper = $app->getServiceManager()->get('filterChainerCustom');
+    $stripChainerFactory = new \Services\Filter\Helper\FilterChainerFactory(); 
+    $BLL = $app->getBLLManager()->get('sysBranchesDealersDeffBLL');  
+    $headerParams = $app->request()->headers();
+    if(!isset($headerParams['X-Public'])) throw new Exception ('rest api "pkInsertAct_sysbranchesdealersdeff" end point, X-Public variable not found');    
+     $pk =  $headerParams['X-Public'];
+      
+    $vName = NULL;
+    if (isset($_GET['name'])) {
+         $stripper->offsetSet('name',$stripChainerFactory->get(stripChainers::FILTER_PARANOID_LEVEL2,
+                                                $app,
+                                                $_GET['name']));
+    }  
+    $AccBodyTypeId = NULL;
+    if (isset($_GET['acc_body_type_id'])) {
+         $stripper->offsetSet('acc_body_type_id',$stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED,
+                                                $app,
+                                                $_GET['acc_body_type_id']));
+    } 
+     
+    $stripper->strip();
+    if($stripper->offsetExists('name')) $vName = $stripper->offsetGet('name')->getFilterValue(); 
+    if($stripper->offsetExists('acc_body_type_id')) $AccBodyTypeId = $stripper->offsetGet('acc_body_type_id')->getFilterValue();
+          
+    $resDataInsert = $BLL->insertAct(array(
+            'Name' => $vName,   
+            'AccBodyTypeId' => $AccBodyTypeId,  
+            'pk' => $pk));
+        
+    $app->response()->header("Content-Type", "application/json"); 
+    $app->response()->body(json_encode($resDataInsert));
+    
+}
+);
+
+/**
+ *  * Okan CIRAN
+ * @since 15-08-2018
+ */ 
+$app->get("/pkUpdateAct_sysbranchesdealersdeff/", function () use ($app ) {
+    $stripper = $app->getServiceManager()->get('filterChainerCustom');
+    $stripChainerFactory = new \Services\Filter\Helper\FilterChainerFactory(); 
+    $BLL = $app->getBLLManager()->get('sysBranchesDealersDeffBLL');  
+    $headerParams = $app->request()->headers();
+    if(!isset($headerParams['X-Public'])) throw new Exception ('rest api "pkUpdateAct_sysbranchesdealersdeff" end point, X-Public variable not found');    
+    $pk = $headerParams['X-Public'];
+    
+    $vId = NULL;
+    if (isset($_GET['id'])) {
+         $stripper->offsetSet('id',$stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED,
+                                                $app,
+                                                $_GET['id']));
+    } 
+    $vName = NULL;
+    if (isset($_GET['name'])) {
+         $stripper->offsetSet('name',$stripChainerFactory->get(stripChainers::FILTER_PARANOID_LEVEL2,
+                                                $app,
+                                                $_GET['name']));
+    }  
+    $AccBodyTypeId = NULL;
+    if (isset($_GET['acc_body_type_id'])) {
+         $stripper->offsetSet('acc_body_type_id',$stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED,
+                                                $app,
+                                                $_GET['acc_body_type_id']));
+    } 
+     
+    $stripper->strip();
+    if($stripper->offsetExists('name')) $vName = $stripper->offsetGet('name')->getFilterValue(); 
+    if($stripper->offsetExists('acc_body_type_id')) $AccBodyTypeId = $stripper->offsetGet('acc_body_type_id')->getFilterValue();
+    if($stripper->offsetExists('id')) $vId = $stripper->offsetGet('id')->getFilterValue();
+     
+          
+    $resDataInsert = $BLL->updateAct(array(
+            'Id' => $vId,   
+            'Name' => $vName,   
+            'AccBodyTypeId' => $AccBodyTypeId,  
+            'pk' => $pk));
+        
+    $app->response()->header("Content-Type", "application/json"); 
+    $app->response()->body(json_encode($resDataInsert));
+    
+}
+);
+ 
+/**
+ *  * Okan CIRAN
+ * @since 15-08-2018
+ */
+$app->get("/pkDeletedAct_sysbranchesdealersdeff/", function () use ($app ) {
+    $stripper = $app->getServiceManager()->get('filterChainerCustom');
+    $stripChainerFactory = new \Services\Filter\Helper\FilterChainerFactory();    
+    $BLL = $app->getBLLManager()->get('sysBranchesDealersDeffBLL');   
+    $headerParams = $app->request()->headers();
+    $Pk = $headerParams['X-Public'];  
+    $vId = NULL;
+    if (isset($_GET['id'])) {
+        $stripper->offsetSet('id', $stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED,
+                                                $app,
+                                                $_GET['id']));
+    } 
+    $stripper->strip(); 
+    if ($stripper->offsetExists('id')) {$vId = $stripper->offsetGet('id')->getFilterValue(); }  
+    $resDataDeleted = $BLL->deletedAct(array(                  
+            'id' => $vId ,    
+            'pk' => $Pk,        
+            ));
+    $app->response()->header("Content-Type", "application/json"); 
+    $app->response()->body(json_encode($resDataDeleted));
+}
+); 
+
 
  
 
