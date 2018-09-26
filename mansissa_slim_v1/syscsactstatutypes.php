344d77fdcf57b793b5f563e08df481da12b0e572
Oki Oki <ociran@gmail.com>
2018-09-26 17:50:15 +0300

    test

--- mansissa_slim_v1/syscsactstatutypes.php
+++ mansissa_slim_v1/syscsactstatutypes.php
@@ -87,10 +87,7 @@ $app->get("/pkCsActStatutypesDdList_syscsactstatutypess/", function () use ($app
             "selected" => false,
             "description" => $flow["name_eng"],
             "imageSrc"=>"",              
-            "attributes" => array( 
-                                    "active" => $flow["active"], 
-                   
-                ),
+            
         );
     }
     $app->response()->header("Content-Type", "application/json");
