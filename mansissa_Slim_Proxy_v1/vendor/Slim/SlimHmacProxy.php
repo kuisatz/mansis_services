94d19c13413a10f23df198e29b194103b376cdd1
Oki Oki <ociran@gmail.com>
2018-09-27 10:42:06 +0300

    pkFillKpnumbersGridx_syskpnumbers

--- mansissa_Slim_Proxy_v1/vendor/Slim/SlimHmacProxy.php
+++ mansissa_Slim_Proxy_v1/vendor/Slim/SlimHmacProxy.php
@@ -24,6 +24,7 @@ class SlimHmacProxy extends \Proxy\Proxy {
         
         //**---- sysKpnumbersBLL ------------------- 
                                     'pkKpnumbersDdList_syskpnumbers' => 'restApiDefaultCall', 
+                                    'pkFillKpnumbersGridx_syskpnumbers' => 'restApiDefaultCall', 
             
          //** sysKpnumbersBLL ----------------------//  
         
