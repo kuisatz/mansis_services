0a908a2ad68514249876937b2e7c14a6a94ae6de
Oki Oki <ociran@gmail.com>
2018-10-04 10:44:57 +0300

    pkUpdateMakeActiveOrPassive_sysbuybackmatrix

--- mansissa_slim_v1/sysbuybackmatrix.php
+++ mansissa_slim_v1/sysbuybackmatrix.php
@@ -292,75 +292,7 @@ $app->get("/pkFillTradebackMatrixGridx_sysbuybackmatrix/", function () use ($app
  *  * Okan CIRAN
  * @since 15-08-2018
  */
-$app->get("/pkUpdateBBMakeActiveOrPassive_sysbuybackmatrix/", function () use ($app ) {
-    $RedisConnect = $app->getServiceManager()->get('redisConnectFactory');
- 
-    $rid = 'testInstance7794f89a-59a3-44f8-b2f8-1e44dc8a6f34';
-    $user = $RedisConnect->hGetAll($rid);
-    //   echo "Server is running: ".$RedisConnect->ping(); 
-
- //   print_r($user);
-    if (isset($user['data']) && $user['data'] != "") {
-        $user = trim($user['data']);
-
-        $jsonFilter = json_decode($user, true);
-        if ($jsonFilter != null) {
-    //        print_r("<<<<<<<<<<<<<<<<<<<<");
-            if (isset($jsonFilter['Id'])) {
-           //     print_r($jsonFilter ["Id"]);
-            }
-    ///        print_r(">>>>>>>>><<<<<<<<<<<");
-            if (isset($jsonFilter['RootId'])) {
-      //          print_r($jsonFilter ["RootId"]);
-            }
-      //      print_r(">>>>>>>>>>>>>>>>>");
-            if (isset($jsonFilter['RoleId'])) {
-       //         print_r($jsonFilter ["RoleId"]);
-            }
-        //    print_r(">>>>>>>>>>>>>>>>>");
-        }
-    } else {
-       //  print_r("<<<<<<<3ee3>>>>>>>");
-         //  print_r("<<<<<<<123>>>>>>>"); 
-        ;
-    }
- 
-    $stripper = $app->getServiceManager()->get('filterChainerCustom');
-    $stripChainerFactory = new \Services\Filter\Helper\FilterChainerFactory();
-    $BLL = $app->getBLLManager()->get('sysBuybackMatrixBLL');
-
-    
-    $RedisConnect = $app->getServiceManager()->get('redisConnectFactory');
-      
-    $headerParams = $app->request()->headers();
-    $Pk = $headerParams['X-Public'];
- //   $user = $RedisConnect->hGetAll($Pk);
-
-    $vId = NULL;
-    if (isset($_GET['id'])) {
-        $stripper->offsetSet('id', $stripChainerFactory->get(stripChainers::FILTER_ONLY_NUMBER_ALLOWED, $app, $_GET['id']));
-    }
-
-    $stripper->strip();
-    if ($stripper->offsetExists('id')) {
-        $vId = $stripper->offsetGet('id')->getFilterValue();
-    }
-
-    $resData = $BLL->makeActiveOrPassive(array(
-        'id' => $vId,
-        'pk' => $Pk,
-    ));
-
-    $app->response()->header("Content-Type", "application/json");
-    $app->response()->body(json_encode($resData));
-}
-);
-
-/**x
- *  * Okan CIRAN
- * @since 15-08-2018
- */
-$app->get("/pkUpdateTBMakeActiveOrPassive_sysbuybackmatrix/", function () use ($app ) {
+$app->get("/pkUpdateMakeActiveOrPassive_sysbuybackmatrix/", function () use ($app ) {
     $RedisConnect = $app->getServiceManager()->get('redisConnectFactory');
  
     $rid = 'testInstance7794f89a-59a3-44f8-b2f8-1e44dc8a6f34';
