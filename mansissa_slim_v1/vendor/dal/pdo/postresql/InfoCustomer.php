78979fe4ed3786ec6014fffea3f729341a3a3a47
Oki Oki <ociran@gmail.com>
2018-10-05 17:28:35 +0300

    test

--- mansissa_slim_v1/vendor/dal/pdo/postresql/InfoCustomer.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/InfoCustomer.php
@@ -218,7 +218,7 @@ class InfoCustomer extends \DAL\DalSlim {
                  
                                ";
             $statement = $pdo->prepare($sql);
-           echo debugPDO($sql, $params);
+         // echo debugPDO($sql, $params);
             $statement->execute();
             $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
             $errorInfo = $statement->errorInfo();
@@ -1563,7 +1563,7 @@ class InfoCustomer extends \DAL\DalSlim {
                             embrace_customer_no, 
                             tu_emb_customer_no, 
                             ce_emb_customer_no, 
-                            other_emb_customer_no
+                            other_emb_customer_no,
                             registration_name, 
                             trading_name, 
                             name_short, 
@@ -1626,7 +1626,7 @@ class InfoCustomer extends \DAL\DalSlim {
                            (SELECT last_value FROM info_customer_id_seq)
                                                  )   ";
                     $statement = $pdo->prepare($sql);
-                  echo debugPDO($sql, $params);
+                    //   echo debugPDO($sql, $params);
                     $result = $statement->execute();
                     $errorInfo = $statement->errorInfo();
                     if ($errorInfo[0] != "00000" && $errorInfo[1] != NULL && $errorInfo[2] != NULL)
