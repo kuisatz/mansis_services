f321395447aa23785cbc4008b76997c28b7fd99d
Oki Oki <ociran@gmail.com>
2018-10-08 12:45:29 +0300

    test

--- mansissa_slim_v1/syseducationdefinitions.php
+++ mansissa_slim_v1/syseducationdefinitions.php
@@ -194,7 +194,7 @@ $app->get("/pkFillEducationDefinitionsGridx_syseducationdefinitions/", function 
        $counts = $resTotalRowCount[0]['count'];
       } ELSE  $menus = array();       
 
-//print_r($menus) ;
+-//print_r($menus) ;
       
     $app->response()->header("Content-Type", "application/json");
     $resultArray = array();
@@ -285,7 +285,7 @@ $app->get("/pkInsertAct_syseducationdefinitions/", function () use ($app ) {
       
     $vName = NULL;
     if (isset($_GET['name'])) {
-         $stripper->offsetSet('name',$stripChainerFactory->get(stripChainers::FILTER_PARANOID_JASON_LVL1,
+         $stripper->offsetSet('name',$stripChainerFactory->get(stripChainers::FILTER_PARANOID_LEVEL2,
                                                 $app,
                                                 $_GET['name']));
     }  
