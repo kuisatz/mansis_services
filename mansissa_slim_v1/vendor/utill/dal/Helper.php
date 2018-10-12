4645106ae825160192c387196923cf25f7a21358
Oki Oki <ociran@gmail.com>
2018-10-12 10:21:12 +0300

    test

--- mansissa_slim_v1/vendor/utill/dal/Helper.php
+++ mansissa_slim_v1/vendor/utill/dal/Helper.php
@@ -12,6 +12,10 @@ final class Helper {
     
     
     public static function haveRecord($result = null) {  
+        print_r($result);
+        print_r("<<<<<<");
+        print_r($result['resultSet'][0]['control']);
+        print_r(">>>>>>>>>");
         if(isset($result['resultSet'][0]['control'])) return true;
         return false;
     }
