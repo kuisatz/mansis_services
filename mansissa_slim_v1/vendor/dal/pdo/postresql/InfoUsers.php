cc73f9bb787d290643136136e7f2dffaa3bea754
Oki Oki <ociran@gmail.com>
2018-10-12 10:12:30 +0300

    test

--- mansissa_slim_v1/vendor/dal/pdo/postresql/InfoUsers.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/InfoUsers.php
@@ -1612,7 +1612,7 @@ class InfoUsers extends \DAL\DalSlim {
                   * 
                   */
             $statement = $pdo->prepare($sql);
-            echo debugPDO($sql, $params);
+           //  echo debugPDO($sql, $params);
             $statement->execute();
             $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
             $errorInfo = $statement->errorInfo();
