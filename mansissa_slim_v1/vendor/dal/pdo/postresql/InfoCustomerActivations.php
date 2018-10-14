b6963bbc46b78d7ab9035462b9402fc1e0ad7573
Oki Oki <ociran@gmail.com>
2018-10-14 22:25:22 +0300

    test

--- mansissa_slim_v1/vendor/dal/pdo/postresql/InfoCustomerActivations.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/InfoCustomerActivations.php
@@ -750,7 +750,7 @@ class InfoCustomerActivations extends \DAL\DalSlim {
                 'offset' => $pdo->quote($offset),
             ); 
                 $statement = $pdo->prepare($sql);
-                //  echo debugPDO($sql, $parameters);                
+               echo debugPDO($sql, $params);                
                 $statement->execute();
                 $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
                 $errorInfo = $statement->errorInfo(); 
