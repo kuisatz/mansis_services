3d7630078b239027dbf70c7a32803b0e248692b5
Oki Oki <ociran@gmail.com>
2018-10-14 19:02:42 +0300

    test

--- mansissa_slim_v1/vendor/dal/pdo/postresql/InfoCustomer.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/InfoCustomer.php
@@ -909,9 +909,10 @@ class InfoCustomer extends \DAL\DalSlim {
                     LEFT JOIN sys_specific_definitions sd16x ON sd16x.language_id = lx.id AND (sd16x.id = sd16.id OR sd16x.language_parent_id = sd16.id) AND sd16x.deleted = 0 AND sd16x.active = 0
                     
                     WHERE  
+                         " . $addSql . "
                         a.deleted =0 AND
                         a.show_it =0  
-                " . $addSql . "
+               
                 " . $sorguStr . " 
                 /*  ORDER BY    " . $sort . " "
                     . "" . $order . " "
@@ -927,7 +928,7 @@ class InfoCustomer extends \DAL\DalSlim {
                 'offset' => $pdo->quote($offset),
             ); 
                 $statement = $pdo->prepare($sql);
-                //  echo debugPDO($sql, $parameters);                
+               echo debugPDO($sql, $params);                
                 $statement->execute();
                 $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
                 $errorInfo = $statement->errorInfo(); 
