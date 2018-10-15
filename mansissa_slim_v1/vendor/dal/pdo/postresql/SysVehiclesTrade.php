c8c9503a1202fe94945e6eb7ff0cb3eb21c38f12
Oki Oki <ociran@gmail.com>
2018-10-15 10:28:55 +0300

    test

--- mansissa_slim_v1/vendor/dal/pdo/postresql/SysVehiclesTrade.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/SysVehiclesTrade.php
@@ -759,7 +759,7 @@ class SysVehiclesTrade extends \DAL\DalSlim {
                 'offset' => $pdo->quote($offset),
             ); 
                 $statement = $pdo->prepare($sql);
-                  echo debugPDO($sql, $params);                
+              //    echo debugPDO($sql, $params);                
                 $statement->execute();
                 $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
                 $errorInfo = $statement->errorInfo(); 
