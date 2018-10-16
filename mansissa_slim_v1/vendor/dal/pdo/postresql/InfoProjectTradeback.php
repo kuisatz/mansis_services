db2e0e882e4c5c7b48d2cfefd0f14984e2d080cc
Oki Oki <ociran@gmail.com>
2018-10-16 08:17:14 +0300

    test

--- mansissa_slim_v1/vendor/dal/pdo/postresql/InfoProjectTradeback.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/InfoProjectTradeback.php
@@ -589,7 +589,7 @@ class InfoProjectTradeback extends \DAL\DalSlim {
  
                                  " ;
              $statement = $pdo->prepare($sql);
-         echo debugPDO($sql, $params);
+        // echo debugPDO($sql, $params);
             $statement->execute();
             $result = $statement->fetchAll(\PDO::FETCH_ASSOC); 
             $errorInfo = $statement->errorInfo();
