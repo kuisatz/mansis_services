fb8b21f539bfb3b31ceb9b929eb374ccd518054c
Oki Oki <ociran@gmail.com>
2018-10-16 08:19:10 +0300

    pkProjectVehicleModelsTradeDdList_infoprojectbuybacks

--- mansissa_slim_v1/vendor/dal/pdo/postresql/InfoProjectBuyback.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/InfoProjectBuyback.php
@@ -587,7 +587,7 @@ class InfoProjectBuyback extends \DAL\DalSlim {
  
                                  " ;
              $statement = $pdo->prepare($sql);
-         echo debugPDO($sql, $params);
+          echo debugPDO($sql, $params);
             $statement->execute();
             $result = $statement->fetchAll(\PDO::FETCH_ASSOC); 
             $errorInfo = $statement->errorInfo();
