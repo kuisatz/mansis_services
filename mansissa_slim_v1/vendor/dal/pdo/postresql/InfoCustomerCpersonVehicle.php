4fc0aa949de48387e6c18402178f49244a1a455b
Oki Oki <ociran@gmail.com>
2018-10-19 09:58:27 +0300

    test

--- mansissa_slim_v1/vendor/dal/pdo/postresql/InfoCustomerCpersonVehicle.php
+++ mansissa_slim_v1/vendor/dal/pdo/postresql/InfoCustomerCpersonVehicle.php
@@ -547,7 +547,7 @@ class InfoCustomerCpersonVehicle extends \DAL\DalSlim {
                     active = 1 ,
                     deleted= 1,
                     show_it =1 
-               WHERE active = 0 AND customer_contact_persons_id = :id  ");
+               WHERE active = 0 AND id = :id  ");
             $statement->bindValue(':id', $params['id'], \PDO::PARAM_INT);
             $update = $statement->execute();
             $afterRows = $statement->rowCount();
